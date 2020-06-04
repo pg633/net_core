e307753036ce11ad2074bae19757036fa2cc3afb
lianzheng04 <lianzheng04@meituan.com>
2020-06-04 20:07:19 +0800

    asd

--- md/a.cc
+++ md/a.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "pg633/net_core/md/a.h"
+#include "pg633/test_a/src/hello.h"
 using namespace std;
 
 int main()
@@ -8,6 +9,8 @@ int main()
     cout << t.get() << endl;
     cout << "asd" << endl;
     cout<< t.get()<<endl;
+    test_a tt;
+    cout<< tt.get()<<endl;
     // cout<<person<<endl;
     // cout<<person.get_name()<<endl;
 }
